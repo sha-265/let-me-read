7d3b6e6d482311d43bcd5cac907aa26b502102fe
sha-265 <sha@mempool.party>
2024-03-05 03:11:33 +0200

    Fix

--- background.js
+++ background.js
@@ -11,7 +11,7 @@ browser.webRequest.onBeforeSendHeaders.addListener(
     function(details) {
         for (var i = 0; i < details.requestHeaders.length; ++i) {
             if (details.requestHeaders[i].name === 'User-Agent') {
-                details.requestHeaders[i].value = 'Mozilla/5.0 (compatible; SemrushBot; +http://www.semrush.com/bot.html)';
+                details.requestHeaders[i].value = 'Mozilla/5.0 (Java) outbrain';
                 break;
             }
         }
